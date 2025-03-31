1aa71d463d3566084c9aa2729210250c4dc990f1
Howard Shen <howardshen2306@gmail.com>
2025-03-31 20:43:50 +0800

    refactor: unify request handling by introducing DTOs for meme coin operations

--- internal/dtos/meme_coin_request.go
+++ internal/dtos/meme_coin_request.go
@@ -3,3 +3,8 @@ package dtos
 type UpdateMemeCoinRequest struct {
 	Description string `json:"description"`
 }
+
+type CreateMemeCoinRequest struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
